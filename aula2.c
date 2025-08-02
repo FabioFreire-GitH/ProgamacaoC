a70a80e64bfd2bbf0cb53de30719aa9f613a3980
FabioFreire-GitH <freirefabio.ff@gmail.com>
2025-08-02 19:36:04 +0000

    Aulas introdutorias

--- aula2.c
+++ aula2.c
@@ -1,1 +1,28 @@
+#include <stdio.h>
+
+int main(){
+    int idade;
+    float altura;
+    char opcao;
+    char nome[20];
+
+printf("Digite sua idade: ");    
+scanf("%d", &idade);
+printf("Sua idade é: %d\n", idade);
+
+printf("Digite a sua altura: ");
+scanf("%f", &altura);
+printf("A altura é: %f\n", altura);
+
+printf("Digite seu nome: ");
+scanf("%s", nome);
+printf("O nome é: %s\n", nome);
+
+printf("Digite a opção: ");
+scanf(" %c", &opcao);
+printf("A opção é: %c\n", opcao);
+
+
+return 0;
+}
 
