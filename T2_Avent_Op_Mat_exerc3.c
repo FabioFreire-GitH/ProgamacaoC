06a29a4c847cd45dfb85ca47d648e6ae7dab1182
FabioFreire-GitH <freirefabio.ff@gmail.com>
2025-08-23 23:31:44 +0000

    incremento e decremento

--- T2_Avent_Op_Mat_exerc3.c
+++ T2_Avent_Op_Mat_exerc3.c
@@ -1,1 +1,44 @@
+#include <stdio.h>
 
+int main(){
+    /*
+    Incremento (++)
+    Pré-Incremento ++a
+    Pós-Incremento a++
+    Decremento (--)
+    Pré-Decremento --a
+    Pós-Decremento a--
+    */
+
+    int numero1 = 1, resultado;
+
+    printf("Antes Incremento: %d\n", numero1);
+    numero1++; 
+    // numero1 = numero1 + 1;
+    // numero1 += 1;
+    printf("Após Incremento: %d\n", numero1);
+
+    numero1--; 
+    // numero1 = numero1 - 1;
+    // numero1 -= 1;
+    printf("Após Decremento: %d\n", numero1);
+
+    resultado = numero1++;
+    printf("Após Pós-Incremento - Numero 1: %d - Resultado: %d\n", numero1, resultado);
+    //Pos incremeto: 
+    //resultado = numero1
+    //numero1++
+
+    resultado = ++numero1;
+    printf("Após Pré-Incremento - Numero 1: %d - Resultado: %d\n", numero1, resultado);
+    //Pre incremeto: 
+    //numero1++
+    //resultado = numero1
+    
+    resultado = numero1--;
+    printf("Após Pós-Decremento - Numero 1: %d - Resultado: %d\n", numero1, resultado);
+
+    resultado = --numero1;
+    printf("Após Pré-Decremento - Numero 1: %d - Resultado: %d\n", numero1, resultado);
+
+}
