ca0261f9f5ed498ba945aaf3eef9a9098ee5a14e
FabioFreire-GitH <freirefabio.ff@gmail.com>
2025-09-06 21:40:13 +0000

    Exercicio

--- T2_Avent_Manip_Exerc1.c
+++ T2_Avent_Manip_Exerc1.c
@@ -1,1 +1,16 @@
+#include <stdio.h>
+ 
+int main() {
+    int a = 10;
+    float b = 3.5;
+    float resultado = a + b; //'a' é convertido implicitamente  para float
+    int x = 10;
+    int y = 3;
+    float quociente = (float) x / y; //'a'é explicitamente convertido  pdo para float
 
+    printf("Resultado: %.2f\n", resultado);
+    printf("Quociente: %.2f\n", quociente);
+
+    return 0;
+
+}
