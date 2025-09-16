3a4021c6a4f87c912e44bcf5916b363779bdd37d
FabioFreire-GitH <freirefabio.ff@gmail.com>
2025-09-16 00:22:04 +0000

    Exercicio 2 T3_Switch_Ex2

---
+++ Tema_3/T3_Switch_Ex2.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+int main() {
+    //jogo da adivinhação
+    int opcao;
+    int numeroSecreto, palpite;
+
+    printf("Menu Principal\n");
+    printf("1. Iniciar Jogo\n");
+    printf("2. Ver Regras\n");
+    printf("3. Sair\n");
+    printf("Escolha uma opcao (1-3): ");
+    scanf("%d", &opcao);
+
+    switch (opcao) {
+        case 1:
+            srand(time(0)); // Inicializa o gerador de numeros aleatorios
+            numeroSecreto = rand () % 10; // Gera um numero aleatorio entre 0 e 9
+            printf ("Digite um numero (0-9): ");
+            scanf ("%d", &palpite);
+            if (palpite == numeroSecreto) {
+                printf ("Parabens! Voce acertou o numero secreto!\n");
+            } else {
+                printf ("Que pena! Voce errou. O numero secreto era %d.\n", numeroSecreto);
+            }
+            printf("Numero secreto %d\n", numeroSecreto); // Apenas para teste, remova esta linha no jogo real
+            break;
+        case 2:
+            printf("Explicacao das regras do jogo:\n");
+            break;
+        case 3:
+            printf("Saindo do jogo. Ate mais!\n");
+            break;
+        default:
+            printf("Opcao invalida!\n");
+            break;
+    }
+}
