83701023da0868f414db48c990e764a0d00171b4
FabioFreire-GitH <freirefabio.ff@gmail.com>
2025-10-09 01:07:30 +0000

    aula

---
+++ Tema_4/T4_For_Aula1.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main() {
+
+    //Sintaxe do for (inicialização; condição; incremento) 
+    for (int i = 1; i <= 5; i++) {
+        printf("%d\n", i);
+    }
+ 
+    return 0;
+}
