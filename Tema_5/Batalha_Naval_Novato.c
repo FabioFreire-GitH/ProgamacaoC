3c2ffb3c2bbc59fa68ea2df625c491cebce0725f
FabioFreire-GitH <freirefabio.ff@gmail.com>
2025-10-21 01:01:32 +0000

    Incluido os navios no tabuleiro

--- Tema_5/Batalha_Naval_Novato.c
+++ Tema_5/Batalha_Naval_Novato.c
@@ -24,8 +24,34 @@ int main () {
         }
         printf("\n");
     }
-    return 0;
+    printf("\n");
 
-    
+    // incluir navios na vertical no tabuleiro com valor '3' e com tres casas
+    tabuleiro[0][0] = 3; // Navio na posição A1
+    tabuleiro[1][0] = 3; // Navio na posição B1
+    tabuleiro[2][0] = 3; // Navio na posição C1
+
+   // incluir navios na horizontal no tabuleiro com valor '3' e com tres casas
+    tabuleiro[4][4] = 3; // Navio na posição E5
+    tabuleiro[4][5] = 3; // Navio na posição E6
+    tabuleiro[4][6] = 3; // Navio na posição E7 
+
+    // Exibe o tabuleiro atualizado com navios
+    printf("Tabuleiro com Navios:\n");
+    printf("  ");
+    for (int j = 0; j < 10; j++) { // Cabeçalho das colunas
+        printf(" %d", coluna[j]);
+    }
+    printf("\n");
+    for (int i = 0; i < 10; i++) { // Linhas do tabuleiro
+        printf("%c ", linha[i]);
+        for (int j = 0; j < 10; j++) {
+            printf(" %d", tabuleiro[i][j]);
+        }
+        printf("\n");   
+    }
+ 
+
+    return 0;
 
 }
